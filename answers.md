b5fc09932c9d81d8bdc12f9046386cc0387111bd
Maxim Armando Duque <duquema@s.dcsdk12.org>
2020-02-25 08:02:41 -0700

    Finished a5

---
+++ answers.md
@@ -0,0 +1,7 @@
+1. Open Picture.java and look for the method getPixels2D. Is it there?  No
+2. Open SimplePicture.java and look for the method getPixels2D. Is it there?  yes
+3. Does the following code compile?       DigitalPicture p = new DigitalPicture(); no it is an interface
+4. Assuming that a no-argument constructor exists for SimplePicture, would the following code compile?        DigitalPicture p = new SimplePicture(); yes
+5. Assuming that a no-argument constructor exists for Picture, does the following code compile?        DigitalPicture p = new Picture(); yes
+6. Assuming that a no-argument constructor exists for Picture, does the following code compile?        SimplePicture p = new Picture(); yes
+7. Assuming that a no-argument constructor exists for SimplePicture, does the following code compile?        Picture p = new SimplePicture(); no
--- src/Picture.java
+++ src/Picture.java
@@ -95,6 +95,52 @@ public class Picture extends SimplePicture {
 		}
 	}
 
+	public void keepOnlyBlue(){
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels){
+			for (Pixel pixelObj : rowArray){
+				pixelObj.setRed(0);
+				pixelObj.setGreen(0);
+			}
+		}
+	}
+	
+	public void negate(){
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels){
+			for (Pixel pixelObj : rowArray){
+				pixelObj.setRed(255 - pixelObj.getRed());
+				pixelObj.setBlue(255 - pixelObj.getBlue());
+				pixelObj.setGreen(255 - pixelObj.getGreen());
+			}
+		}
+	}
+
+	public void grayscale(){
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels){
+			for (Pixel pixelObj : rowArray){
+				int average = (pixelObj.getRed() + pixelObj.getBlue() + pixelObj.getGreen()) / 3;
+				pixelObj.setRed(average);
+				pixelObj.setBlue(average);
+				pixelObj.setGreen(average);
+			}
+		}
+	}
+
+	public void fixUnderwater(){
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels){
+			for (Pixel pixelObj : rowArray){
+				int blueAmount = pixelObj.getBlue();
+				if (blueAmount < 170){
+					pixelObj.setBlue(0);
+				}
+			}
+		}
+	}
+
+
 	/**
 	 * Method that mirrors the picture around a vertical mirror in the center of
 	 * the picture from left to right
@@ -204,10 +250,10 @@ public class Picture extends SimplePicture {
 	 * Main method for testing - each class in Java can have a main method
 	 */
 	public static void main(String[] args) {
-		Picture beach = new Picture("beach.jpg");
-		beach.explore();
-		beach.zeroBlue();
-		beach.explore();
+		Picture water = new Picture("water.jpg");
+		water.explore();
+		water.zeroBlue();
+		water.explore();
 	}
 
 } // this } is the end of class Picture, put all new methods before this
